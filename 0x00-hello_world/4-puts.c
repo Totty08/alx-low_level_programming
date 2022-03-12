c886e65d6567dbac68ca22e9b6c6608090f6ef28
Totty08 <olibanje@gmail.com>
2022-03-12 10:18:35 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "programming is like building a multiple puzzle"
+ * retun: 0 if the properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like buildong a multilingual puzzle");
+	return (0);
+}
+
