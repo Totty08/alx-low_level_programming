19108048015d9e9f820f7e77a8b6af26d9c491d3
Totty08 <olibanje@gmail.com>
2022-03-18 06:46:43 -0700

    numbers speak

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
+
 /**
- * print_numbers - print numbers
- * Return: Always 0.
+ * print_numbers - a function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ * Return: Always 0
  */
 void print_numbers(void)
 {
-int i;
+	int i = '0';
 
-i = 0;
-while (i < 10)
-{
-_putchar (i + '0');
-i++;
-}
-
-_putchar ('\n');
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
