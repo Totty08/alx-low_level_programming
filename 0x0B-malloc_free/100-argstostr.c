e0e157dff4c5dcf9839e8bd50acbfefa77a744c1
Totty08 <olibanje@gmail.com>
2022-04-05 13:28:49 -0700

    concatenates all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,32 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * argstostr - prints args
- * @ac: takes in width of grid
- * @av: height of grid
- * Return: the args one line at a time
+ * argstostr - concatenates arguments.
+ * @ac: number of string to be concatenated.
+ * @av: strings to be concatenated.
+ * Return: String concatenated.
  */
-
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int count = 0, a = 0, b = 0, c = 0;
+int i, j, k = 0;
+char *concat = NULL;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	while (a < ac)
-	{
-		b = 0;
-		while (av[a][b] != '\0')
-		{
-			count++;
-			b++;
-		}
-		a++;
-	}
-	count = count + ac + 1;
-	str = malloc(sizeof(char) * count);
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (a = 0; a < ac; a++)
-	{
-		for (b = 0; av[a][b] != '\0'; b++)
-		{
-			str[c] = av[a][b];
-			c++;
-		}
-		str[c] = '\n';
-		c++;
-	}
-	return (str);
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+for (j = 0; av[i][j] != '\0'; j++)
+k++;
+concat = malloc(sizeof(char) * (k + ac + 1));
+if (concat == NULL)
+return (NULL);
+for (i = 0, k = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++, k++)
+concat[k] = av[i][j];
+concat[k] = '\n';
+k++;
 }
+concat[k] = '\0';
+return (concat);
+}
+
