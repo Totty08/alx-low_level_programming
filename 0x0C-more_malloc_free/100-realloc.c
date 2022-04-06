23abb58fa3f49f4f21066498340fcb1bf2f812c1
Totty08 <olibanje@gmail.com>
2022-04-06 11:37:15 -0700

    rellocates a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,29 +3,41 @@
 #include <stdlib.h>
 
 /**
- * array_range - Entry point
- *@min: number min
- *@max: number max
+ * _realloc - Entry point
+ *@ptr: pointer to the memory previously allocated
+ *@old_size: size of the allocated ptr
+ *@new_size: size of new memory block
  * Return: a pointer to an array
  */
-int *array_range(int min, int max)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int *array = NULL;
-	int i, t, count;
+	char *array = NULL;
+	char *p = NULL;
+	unsigned int i;
+	char *px = NULL;
 
-	count = 0;
-	if (min > max)
-		return (NULL);
-	t = (max - min) + 1;
-	array = malloc(t * sizeof(int));
-	if (array != NULL)
+	if (ptr == NULL)
 	{
-		for (i = min; i <= max; i++)
-		{
-			array[count] = i;
-			count++;
-		}
+		array = malloc(new_size);
+		if (array == NULL)
+			return (NULL);
 		return (array);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size ==  old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p != NULL)
+	{
+		px = (char *)ptr;
+		for (i = 0; i < old_size; i++)
+			p[i] = px[i];
+		free(ptr);
+		return (p);
 	}
 	return (NULL);
 }
